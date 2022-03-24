20bac5c3739ed2f80f6edc4c7f210e03a4dd421b
LMrAllan <alan123arafer@gmail.com>
2022-03-24 17:20:48 -0300

    primeira página

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+function Home(){
+    return (
+        <div>
+            <h2>Finalmente!!!</h2>
+            <Contador></Contador>
+            <h3>É isso!!!</h3>
+        </div>
+    )
+}
+
+function Contador(){
+    const [contador, setcontador] = useState(1);
+   
+    function adicionarContador(){
+        setcontador(contador + 1);
+    }    
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+        </div>
+    )
+}
+
+export default Home
